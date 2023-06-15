5f3625f1025d1e12e4782ed8bf8069943522ad15
nick <34077014+nockwilde@users.noreply.github.com>
2023-06-15 17:39:06 +0100

    global styles

--- src/stitches/StyleSheet.tsx
+++ src/stitches/StyleSheet.tsx
@@ -1,6 +1,16 @@
 import {getCssText, globalCss} from 'stitches-config';
 import { normalize } from 'stitches-normalize-css';
 
-globalCss(...normalize)();
+globalCss(...[
+  ...normalize,
+  {
+    'body': {
+      'margin': 0,
+    },
+    '*, *::before, *::after' : {
+      boxSizing: 'border-box',
+    },
+  },
+])();
 
 export const StyleSheet = () => ( <style id='stitches' dangerouslySetInnerHTML={{__html: getCssText()}}/> )
