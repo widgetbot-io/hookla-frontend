604d423d36dffdbefafc676fd39b3ecf012f6518
nick <34077014+nockwilde@users.noreply.github.com>
2023-07-05 19:31:29 +0100

    responsive container

--- src/components/common/Container.tsx
+++ src/components/common/Container.tsx
@@ -3,4 +3,16 @@ import {styled} from 'stitches-config';
 export const Container = styled('div', {
   maxWidth: '1284px',
   margin: '0 auto',
+
+  '@media (max-width: 1426px)': {
+    maxWidth: '90%',
+  },
+
+  '@media (max-width: 800px)': {
+    maxWidth: '720px',
+  },
+
+  '@media (max-width: 758px)': {
+    maxWidth: '95%',
+  },
 });
