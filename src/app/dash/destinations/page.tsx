16168be97857fd63dc50213b0818846261012370
nick <34077014+nockwilde@users.noreply.github.com>
2023-07-19 21:58:04 +0100

    destinations dash

--- src/app/dash/destinations/page.tsx
+++ src/app/dash/destinations/page.tsx
@@ -1,7 +1,32 @@
+import * as Dash from "@/components/dash";
+import * as Common from "@/components/common";
+import {theme} from "stitches-config";
+
 export default function Page() {
   return (
     <>
-      Destinations
+      <Dash.Heading>
+        <Common.Text size='h3' as='h1'>Destinations.</Common.Text>
+        <Common.Text css={{color: theme.colors.neutral700}}>Define your Destinations for the WidgetBot webhook here.</Common.Text>
+      </Dash.Heading>
+      <Dash.Section title='Destinations.' desc='Destinations description here.'>
+        <Dash.Route>
+          <Common.Text>Destination</Common.Text>
+          <Common.Text>Destination Description</Common.Text>
+        </Dash.Route>
+        <Dash.Route>
+          <Common.Text>Destination</Common.Text>
+          <Common.Text>Destination Description</Common.Text>
+        </Dash.Route>
+        <Dash.Route>
+          <Common.Text>Destination</Common.Text>
+          <Common.Text>Destination Description</Common.Text>
+        </Dash.Route>
+        <Dash.Route css={{marginBottom: 0}}>
+          <Common.Text>Destination</Common.Text>
+          <Common.Text>Destination Description</Common.Text>
+        </Dash.Route>
+      </Dash.Section>
     </>
   )
 }
