20c223e63683c053c617ee9286ceb99a83edde07
nick <34077014+nockwilde@users.noreply.github.com>
2023-07-25 13:35:57 +0100

    embed builder

--- src/components/dash/Embed.tsx
+++ src/components/dash/Embed.tsx
@@ -1,10 +1,77 @@
-import {styled} from 'stitches-config';
+import {styled, theme} from 'stitches-config';
+import Link from 'next/link';
 import {Input} from '@/components/dash/Input';
 import {Checkbox} from '@/components/dash/Checkbox';
+import * as Common from '@/components/common';
+
+const EmbedBuilder = styled('section', {
+  '& > *:not(:last-child)': {
+    marginBottom: '8px',
+  }
+});
 
 const EmbedPreview  = styled('section');
 
-const EmbedBuilder = styled('section');
+const EmbedPreviewWrap = styled(Common.Box, {
+  borderStyle: 'dashed',
+  borderColor: theme.colors.neutral300,
+  marginTop: '8px',
+});
+
+const EmbedPreviewBox = styled('div', {
+  border: '1px solid',
+  borderLeft: '4px solid',
+  borderColor: theme.colors.neutral900,
+  borderLeftColor: theme.colors.magenta400,
+  borderRadius: theme.radii.medium,
+  background: theme.colors.neutral800,
+  padding: '16px',
+});
+
+const EmbedHeader = styled('div', {
+  display: 'flex',
+  alignItems: 'center',
+  marginBottom: '8px',
+
+  '& > a': {
+    color: theme.colors.neutral100,
+    textDecoration: 'none',
+  },
+
+  '&::before': {
+    content: '',
+    display: 'block',
+    width: '24px',
+    height: '24px',
+    backgroundImage: 'url(/pfp.jpg)',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center center',
+    backgroundSize: 'cover',
+    borderRadius: theme.radii.circle,
+    marginRight: '8px',
+  }
+});
+
+const EmbedFooter = styled('div', {
+  display: 'flex',
+  alignItems: 'center',
+  marginTop: '8px',
+  color: theme.colors.neutral300,
+  fontSize: theme.fontSizes.xsmall,
+
+  '&::before': {
+    content: '',
+    display: 'block',
+    width: '20px',
+    height: '20px',
+    backgroundImage: 'url(/github.jpg)',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center center',
+    backgroundSize: 'cover',
+    borderRadius: theme.radii.circle,
+    marginRight: '8px',
+  }
+});
 
 export function Embed() {
   return (
@@ -17,7 +84,18 @@ export function Embed() {
         <Checkbox label='Timestamp' />
       </EmbedBuilder>
       <EmbedPreview>
-        Preview
+        <Common.Text as='h3' size='h4' css={{marginTop: '32px'}}>Preview</Common.Text>
+        <EmbedPreviewWrap>
+          <EmbedPreviewBox>
+            <EmbedHeader>
+              <Link href='/'>nick</Link>
+            </EmbedHeader>
+            <Common.Text css={{color: theme.colors.neutral400}}>feat: Create index</Common.Text>
+            <EmbedFooter>
+              <Common.Text>widgetbot-io/hookla-frontend:master &middot; Today at 01:30</Common.Text>
+            </EmbedFooter>
+          </EmbedPreviewBox>
+        </EmbedPreviewWrap>
       </EmbedPreview>
     </>
   )
