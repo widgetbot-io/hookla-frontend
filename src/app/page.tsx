5ed602c35477c6967ddbfea67a0da535dec38f63
JohnyTheCarrot <tuurmartens4@gmail.com>
2023-08-21 10:25:15 +0200

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { css } from "panda/css";
+
+export default function Home() {
+  return (
+    <div className={css({ fontSize: "2xl", fontWeight: "bold" })}>
+      Hello 🐼!
+    </div>
+  );
+}
